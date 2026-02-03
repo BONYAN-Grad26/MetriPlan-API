860ea9fdb2d68848fd712f8500ba28f3d36c0114
MohamedAbdelaziz177 <mgouda@std.mans.edu.eg>
2026-02-03 15:17:17 +0200

    optimizing relationship config..

--- src/main/java/com/abdelaziz26/metriplate/entities/MealIngredient.java
+++ src/main/java/com/abdelaziz26/metriplate/entities/MealIngredient.java
@@ -1,16 +1,14 @@
 package com.abdelaziz26.metriplate.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity @Table(name = "meal_ingredients")
+@Builder
 public class MealIngredient {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
